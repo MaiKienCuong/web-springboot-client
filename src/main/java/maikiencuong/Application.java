2f671d8ab2464878d2a1c21364d17d0adef0090a
maikiencuong <maikiencuongiuh@gmail.com>
2021-06-12 23:46:31 +0700

    initial commit

---
+++ src/main/java/maikiencuong/Application.java
@@ -0,0 +1,28 @@
+package maikiencuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("faildMessage");
+		return messageSource;
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
